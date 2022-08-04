fab481d86845dd8c4dfd6a110475cea0b7b7c434
nguyenthiquynhnhu060320 <70968782+nguyenthiquynhnhu060320@users.noreply.github.com>
2022-08-04 09:41:39 +0700

    Create Project

---
+++ DEMOQLNT/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace DEMOQLNT
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.slidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menubutton = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btConDan = new System.Windows.Forms.Button();
+            this.SlideTimer = new System.Windows.Forms.Timer(this.components);
+            this.slidebar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.menubutton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // slidebar
+            // 
+            this.slidebar.BackColor = System.Drawing.Color.White;
+            this.slidebar.Controls.Add(this.panel1);
+            this.slidebar.Controls.Add(this.btConDan);
+            this.slidebar.Controls.Add(this.button6);
+            this.slidebar.Controls.Add(this.button5);
+            this.slidebar.Controls.Add(this.button4);
+            this.slidebar.Controls.Add(this.button2);
+            this.slidebar.Controls.Add(this.button3);
+            this.slidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.slidebar.Location = new System.Drawing.Point(0, 0);
+            this.slidebar.MaximumSize = new System.Drawing.Size(225, 817);
+            this.slidebar.MinimumSize = new System.Drawing.Size(74, 817);
+            this.slidebar.Name = "slidebar";
+            this.slidebar.Size = new System.Drawing.Size(225, 817);
+            this.slidebar.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.menubutton);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 121);
+            this.panel1.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Image = global::DEMOQLNT.Properties.Resources.rsz_1family;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(3, 212);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 12, 3, 3);
+            this.button6.Name = "button6";
+            this.button6.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button6.Size = new System.Drawing.Size(250, 58);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "    Sổ Gia Đình";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Image = global::DEMOQLNT.Properties.Resources.rsz_group;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(3, 285);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 12, 3, 3);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(250, 58);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "    Lớp Học";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Image = global::DEMOQLNT.Properties.Resources.rsz_money;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(3, 358);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 12, 3, 3);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(250, 58);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "    Thu/Chi";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = global::DEMOQLNT.Properties.Resources.report__1_;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(3, 504);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 12, 3, 3);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(250, 58);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "    Báo Cáo";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::DEMOQLNT.Properties.Resources.employee__1_;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(3, 431);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 12, 3, 3);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(250, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "   Nhân Viên";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // menubutton
+            // 
+            this.menubutton.BackgroundImage = global::DEMOQLNT.Properties.Resources.rsz_menu;
+            this.menubutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.menubutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menubutton.Location = new System.Drawing.Point(9, 55);
+            this.menubutton.Name = "menubutton";
+            this.menubutton.Size = new System.Drawing.Size(52, 44);
+            this.menubutton.TabIndex = 0;
+            this.menubutton.TabStop = false;
+            this.menubutton.Click += new System.EventHandler(this.menubutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Menu";
+            // 
+            // btConDan
+            // 
+            this.btConDan.FlatAppearance.BorderSize = 0;
+            this.btConDan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btConDan.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btConDan.Image = global::DEMOQLNT.Properties.Resources.rsz_person;
+            this.btConDan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btConDan.Location = new System.Drawing.Point(3, 139);
+            this.btConDan.Margin = new System.Windows.Forms.Padding(3, 12, 3, 3);
+            this.btConDan.Name = "btConDan";
+            this.btConDan.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btConDan.Size = new System.Drawing.Size(250, 58);
+            this.btConDan.TabIndex = 2;
+            this.btConDan.Text = "    Con Dân";
+            this.btConDan.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btConDan.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btConDan.UseVisualStyleBackColor = true;
+            // 
+            // SlideTimer
+            // 
+            this.SlideTimer.Interval = 10;
+            this.SlideTimer.Tick += new System.EventHandler(this.SlideTimer_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 817);
+            this.Controls.Add(this.slidebar);
+            this.MinimumSize = new System.Drawing.Size(1152, 864);
+            this.Name = "FormMain";
+            this.Text = "Menu";
+            this.slidebar.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.menubutton)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel slidebar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox menubutton;
+        private System.Windows.Forms.Button btConDan;
+        private System.Windows.Forms.Timer SlideTimer;
+    }
+}
+
---
+++ DEMOQLNT/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEMOQLNT
+{
+    public partial class FormMain : Form
+    {
+        bool sidebarExpand;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void SlideTimer_Tick(object sender, EventArgs e)
+        {
+            if(sidebarExpand)
+            {
+                slidebar.Width -= 10;
+                if(slidebar.Width == slidebar.MinimumSize.Width)
+                {
+                    sidebarExpand = false;
+                    SlideTimer.Stop();
+
+                }   
+                else
+                {
+                    slidebar.Width += 10;
+                    if(slidebar.Width == slidebar.MaximumSize.Width)
+                    {
+                        sidebarExpand = true;
+                        SlideTimer.Stop();
+                    }    
+                }    
+            }    
+        }
+
+        private void menubutton_Click(object sender, EventArgs e)
+        {
+            SlideTimer.Start();
+        }
+    }
+}
